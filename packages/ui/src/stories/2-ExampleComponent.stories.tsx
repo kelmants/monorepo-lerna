bb6c779e23d1db30581f11ec6aad01f2789d90fb
kelman celis <kelman.celis@tiendanube.com>
2020-05-13 22:24:10 -0300

    global configurations add

---
+++ packages/ui/src/stories/2-ExampleComponent.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ExampleComponent } from "@kelmants/components";
+
+export default {
+  title: "ExampleComponent",
+  component: ExampleComponent,
+};
+
+export const ExampleComponentDefault = () => (
+  <ExampleComponent text="from  stories" />
+);
